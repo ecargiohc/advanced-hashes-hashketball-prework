0bd9fcd68d223adecbab535497a72c9193aec7dd
Grace Colette <ilovegrace09@gmail.com>
2019-07-06 23:52:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -238,7 +238,6 @@ def winning_team
           attributes.each do |player, player_data|
             home_points << player_data[:points]
       binding.pry
-    end
     elsif team == :away
       data.each do |key, value|
         if key == :players
