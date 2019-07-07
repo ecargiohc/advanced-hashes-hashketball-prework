2e97677c5435e668eb52884d4588b41811e409b6
Grace Colette <ilovegrace09@gmail.com>
2019-07-07 01:01:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -255,7 +255,7 @@ def winning_team
       end
     end
   end
-  if home_points.reduce(:+) > away_points.reduce(:+)
+  # if home_points.reduce(:+) > away_points.reduce(:+)
     
   binding.pry
 end
