12a91bced2c8fe5b30777b10b6aab2b652858815
Grace Colette <ilovegrace09@gmail.com>
2019-07-07 01:36:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -293,7 +293,6 @@ def long_name_steals_a_ton?
             if player_data[:steals] > most_steals
               most_steals = player_data[:steals]
               stealer = player
-            # binding.pry
           end
         end
       end
